0093bc72860b31b5fa12167e2a4da8bc9fbbfa5f
wang_q <857454370@qq.com>
2019-12-04 14:52:37 +0800

    spring boot admin

---
+++ spring-boot2-admin-client/src/main/java/com/boot2/client/AdminClientApplication.java
@@ -0,0 +1,21 @@
+package com.boot2.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈功能概述〉<br>
+ *
+ * @className: AdminClient
+ * @package: com.boot2.client
+ * @author: admin
+ * @date: 2019/12/4 14:17
+ */
+
+@SpringBootApplication
+public class AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientApplication.class, args);
+    }
+}
