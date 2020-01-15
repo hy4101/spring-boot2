c8e478f624c4b2767f5d59ad9813fe10b4db8ca4
wang_q <857454370@qq.com>
2020-01-15 10:10:02 +0800

    tz

---
+++ sb2-mybatis-multiple-datasource/src/main/java/com/mybatis/multiple/datasource/MybatisMDApplication.java
@@ -0,0 +1,21 @@
+package com.mybatis.multiple.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈功能概述〉<br>
+ *
+ * @className: MybatisMDApp
+ * @package: com.mybatis.multiple.datasource
+ * @author: admin
+ * @date: 2020/1/14 16:38
+ */
+@SpringBootApplication
+public class MybatisMDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisMDApplication.class, args);
+    }
+
+}
